1fd15f479b4df39c54656604caa5e9e5984206f1
jnesl <62406670+jakeNesler@users.noreply.github.com>
2022-10-30 00:58:54 -0400

    modified:   node_modules/.package-lock.json

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -6,10 +6,32 @@
 //
 // Scripts
 // 
-var firebase = require('firebase');
-var firebaseui = require('firebaseui');
-
-  
+ 
+import createGlobe from 'cobe'
+const globe = createGlobe(canvas, {
+    devicePixelRatio: 3,
+    width: 600,
+    height: 600,
+    phi: 1,
+    theta: 0.41,
+    dark: 1,
+    diffuse: 1.63,
+    mapSamples: 15000,
+    mapBrightness: 6,
+    baseColor: [0.3, 0.3, 0.3],
+    markerColor: [1, 0.5, 1],
+    glowColor: [1, 1, 1],
+    markers: [ // add custom markers
+      { location: [37.7595, -122.4367], size: 0.03 },
+      { location: [40.7128, -74.006], size: 0.1 },
+    ],
+    onRender: (state) => {
+      // Called on every animation frame.
+      // `state` will be an empty object, return updated params.
+      state.phi = phi
+      phi += 0.01
+    },
+})
 
 window.addEventListener('DOMContentLoaded', event => {
 
